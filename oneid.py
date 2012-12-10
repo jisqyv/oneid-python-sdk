a1476645e5386423a3f599194d081c3cd2820d8d
brianpkennedy <brianpkennedyut@gmail.com>
2012-12-10 15:16:31 -0600

    first checkin stub for python sdk

---
+++ oneid.py
@@ -0,0 +1,50 @@
+import urllib2
+import datetime
+import json
+
+
+
+def _call_OneID():
+    pass
+
+def set_oneid_credentials():
+    pass
+
+def make_nonce():
+    pass
+
+def parse_response():
+    pass
+
+def draw_signin_button(callback_url, attrs="personal_info[email] personal_info[first_name] personal_info[last_name]"):
+    params = json.dumps({
+            "challenge" : {"attr" : attrs, "callback" : callback_url}
+        }
+    )
+    js = "<span class='oneid_login_ctr'></span>"
+    js+= "<script type='text/javascript'>"
+    js+= "OneIdExtern.registerApiReadyFunction(function(){"
+    js+= "OneId.loginButton('.oneid_login_ctr'," + params +")"
+    js+= "})"
+    js+="</script>"
+    
+    return js
+
+def draw_quickfill_button(attrs):
+    js = "<span class='oneid_quickfill_ctr'></span>"
+    js+= "<script type='text/javascript'>"
+    js+= "OneIdExtern.registerApiReadyFunction(function(){"
+    js+= "OneId.accuFillButton('.oneid_quickfill_ctr'," + attrs +")"
+    js+= "})"
+    js+="</script>"
+    
+    return js
+
+def draw_provision_button():
+    pass
+
+def do_redirect():
+    pass
+
+def is_success():
+    pass
